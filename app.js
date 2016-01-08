0e9e04ea7c8a6b9702e443038d3e470cffc8d994
Hugo Roque <hugolnx@gmail.com>
2016-01-08 00:38:27 -0200

    setting up primus and making a first test connection

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
-var express = require('express');
+var express = require('express'),
+    Primus = require("primus");
+
+
 var app = express();
 app.use(express.static('public'));
-
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
+  configWebsocketServerAttachedTo(server);
+
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+function configWebsocketServerAttachedTo(server) {
+  var primus = new Primus(server, { parser: "JSON" } );
+
+  primus.save('public/lib/primus.js');
+
+  primus.on("connection", function(spark) {
+    console.log("connected");
+
+    spark.on("data", function(data) {
+      console.log("received", data);
+      spark.write({serverMessage: "Hello Browser!"});
+    });
+  });
+
+  primus.on("disconnection", function() {
+    console.log("disconnected");
+  });
+
+  primus.on("data", function(message) {
+    console.log("message", message);
+  });
+}
